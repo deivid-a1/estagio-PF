e9dcd47f419ab8f5dc8810a67bdb608b6faa7c8c
Deivid Carvalho <deivid_506_02@hotmail.com>
2020-12-22 11:45:45 -0300

    Atualizando a segunda questão

--- Questao-2.py
+++ Questao-2.py
@@ -5,7 +5,7 @@ data = pd.read_csv('arquivo.csv',sep = ';') #Aqui nós usamos a funçãoo read_c
 
 data = data.sort_values(by=['nome']) #Aqui nós organizamos tudo em função dos nomes, ou seja, em forma alfabética.
 
-print(data) #printamos os registros.
+print(data) #printamos os registros ordenado pelo nome, com o índice informando a posição.
 
 '''
 #No texto da segunda questão não fica claro se quer o retorno da lista ou do registro por inteiro.
@@ -18,7 +18,7 @@ data = pd.read_csv('arquivo.csv',sep = ';') #Aqui nós usamos a funçãoo read_c
 data = data.sort_values(by=['nome']) #Aqui nós organizamos tudo em função dos nomes, ou seja, em forma alfabética.
 
 data = data['nome'].tolist() #isso transformará o csv em uma lista, onde ela estará ordenada em ordem alfabética.
-print(data) #printamos a lista.
+print(data) #printamos a lista, ordenado pelo nome, com o índice informando a posição.
 
 #De acordo com o texto, existem essas duas possibilidades, para testar essa basta descomentar.
 '''
